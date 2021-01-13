e201d52f46565cbfd2d4a51d53a533b1d48bcc67
Adam Tegtmeier <ategtmei@gmail.com>
2021-01-13 11:09:02 -0500

    Finished tutorial and everything seems to be working.

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
+app.debug = True
+
 socketio = SocketIO(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return send_from_directory(".","index.html", as_attachment=False)
+
+@app.route('/socket.io.js')
+def socket_file():
+    return send_from_directory(".","socket.io.js", as_attachment=False)
 
 @socketio.on('my event')
 def test_message(message):
@@ -27,3 +34,4 @@ def test_disconnect():
 
 if __name__ == '__main__':
     socketio.run(app)
+    #app.run()
