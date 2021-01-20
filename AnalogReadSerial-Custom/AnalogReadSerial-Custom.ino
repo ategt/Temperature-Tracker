abbd75ed97272fdff646110dc5e3920fefb10ad9
Adam Tegtmeier <ategtmei@gmail.com>
2021-01-20 11:48:15 -0500

    More aggressive denoising and an updated temperature conversion function.

--- AnalogReadSerial-Custom/AnalogReadSerial-Custom.ino
+++ AnalogReadSerial-Custom/AnalogReadSerial-Custom.ino
@@ -20,7 +20,7 @@ void setup() {
 void loop() {
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-  int readingCount = 50;
+  int readingCount = 100;
 
   // print out the value you read:
   Serial.print(sensorValue);
