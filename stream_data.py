6f0ab7ffbdf2399f4a9c6de828fccf10984f4fa3
Adam Tegtmeier <ategtmei@gmail.com>
2021-02-10 00:59:00 -0500

    Minor hard coding incrementing.

--- stream_data.py
+++ stream_data.py
@@ -7,40 +7,43 @@ import serial
 import time
 import re
 
-try:
-  _serial = serial.Serial("COM4", 9600)
-  sio = socketio.Client()
-  comma = ",".encode()
-  
-  result = _serial.read_all()
-  
-  with open("temp3.txt", 'ab') as handle:
-      handle.write(str(time.time()).encode())
-      handle.write(comma)
-      handle.write(result)
-  
-  if sio:
-      sio.connect("ws://127.0.0.1:5000/")
+def main():
+  try:
+    _serial = serial.Serial("COM4", 9600)
+    sio = socketio.Client()
+    comma = ",".encode()
+    
+    result = _serial.read_all()
+    
+    with open("temp3.txt", 'ab') as handle:
+        handle.write(str(time.time()).encode())
+        handle.write(comma)
+        handle.write(result)
+    
+    if sio:
+        sio.connect("ws://127.0.0.1:5000/")
 
-  with open("temp5.txt", 'ab') as handle:
-    for _ in range(90000):
-            result = _serial.readline()
-            handle.write(str(time.time()).encode())
-            handle.write(comma)
-            handle.write(result)
-            
-            readings = result.split(comma)
-            first_reading = readings[0]
-            avg_reading = readings[-1]
-            
-            try:
-                sio.emit("sensor reading event", {"data":str(avg_reading)})
-            except socketio.exceptions.BadNamespaceError as ex:
-                pass
-            
-            print("\r", first_reading.decode("utf-8"), ",", avg_reading.decode("utf-8").strip(), " - ", randint(0, 250), " " * 25, end='')
-            sleep(0.5)
-finally:            
-  _serial.close()
-  sio.disconnect()
-  
+    with open("temp6.txt", 'ab') as handle:
+      for _ in range(90000):
+              result = _serial.readline()
+              handle.write(str(time.time()).encode())
+              handle.write(comma)
+              handle.write(result)
+              
+              readings = result.split(comma)
+              first_reading = readings[0]
+              avg_reading = readings[-1]
+              
+              try:
+                  sio.emit("sensor reading event", {"data":str(avg_reading)})
+              except socketio.exceptions.BadNamespaceError as ex:
+                  pass
+              
+              print("\r", first_reading.decode("utf-8"), ",", avg_reading.decode("utf-8").strip(), " - ", randint(0, 250), " " * 25, end='')
+              sleep(0.5)
+  finally:            
+    _serial.close()
+    sio.disconnect()
+
+if __name__ == "__main__":
+  main()
